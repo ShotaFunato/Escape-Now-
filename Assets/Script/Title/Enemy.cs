5bcb65ea77b953d326fabb51db7c39c4c7eebef2
ShotaFunato <shota.funato.2013@gmail.com>
2016-12-15 23:45:02 +0900

    ファイル名変更

---
+++ Assets/Script/Title/Enemy.cs
@@ -0,0 +1,221 @@
+﻿/**
+ * 敵クラス
+ * @author Shota Funato
+ */
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FunatoLib;
+
+public class Enemy : Work
+{
+    /// <summary>
+    /// ルーチン１番号の状態列挙
+    /// </summary>
+    private enum StateRoutine1
+    {
+        StateDefault,
+        StateMove,
+
+        StateMax
+    };
+
+    /// <summary>
+    /// 移動待機時間
+    /// </summary>
+    [SerializeField]
+    private float moveWaitTime = 1.0f;
+
+    /// <summary>
+    /// 状態
+    /// </summary>
+    private Enemy.StateRoutine1 stateRoutine1;
+
+    /// <summary>
+    /// ルーチン１番の関数テーブル
+    /// </summary>
+    private delegate void Routine1Func();
+    List<Routine1Func> routineFuncList = new List<Routine1Func>();
+
+    /// <summary>
+    /// ルートを構成する地点リスト
+    /// </summary>
+    List<Vector2> routeWHList = new List<Vector2>();
+
+    /// <summary>
+    /// 現在のルート地点リスト添え字
+    /// </summary>
+    private int nowRouteNum;
+
+    /// <summary>
+    /// ルートを進行させる方向（true = +, false = -）
+    /// </summary>
+    private bool routeDir;
+
+    /// <summary>
+    /// 向いている方向
+    /// </summary>
+    private MapController.Dir dir;
+
+    /// <summary>
+    /// 時間
+    /// </summary>
+    private float time;
+
+    /// <summary>
+    /// ルート決定
+    /// </summary>
+    private void SetRoute()
+    {
+        // マップ構成データ取得
+        MapController mapController = MapController.Instance;
+        List<int> mapLayerData = mapController.GetMapDatas()[(int)MapController.MapLayerKind.Route];
+
+        // ルート判定するための値を最初に略しておく
+        int RouteFirst = (int)MapController.CsvCode.RouteU;
+        int RouteLast = (int)MapController.CsvCode.RouteR;
+        int RouteReturn = (int)MapController.CsvCode.RouteReturn;
+
+        // 現在位置をマップチップの二次元配列上添え字に変換して、最初の地点として登録する
+        Vector2 nowWH = mapController.CalcMapChipHW(this.transform.localPosition);
+        routeWHList.Add(nowWH);
+
+        // マップチップ上のデータを取得するために、レイヤーデータ配列の添え字に変換。現在位置からのルート情報を取得
+        int nowNum = mapController.CalcListWHConvNum(nowWH);
+        MapController.CsvCode routeData = (MapController.CsvCode)mapLayerData[nowNum];
+
+        // 現在位置が折り返し地点なら、そこから繋がる情報を探す
+        if ((int)routeData == RouteReturn)
+        {
+            // 現時点の通過可能情報を取得
+            MapController.PassageRoute passageRoute = mapController.GetPassageRoute(nowWH);
+            int dirNum = RouteLast - RouteFirst;
+            for (int i = 0; i <= dirNum; i++)
+            {
+                if (!passageRoute.dir[i]) continue;
+                Vector2 chkChipNum = nowWH + MapController.RouteChkTbl[i];
+                if ((chkChipNum.x < 0) || (chkChipNum.y < 0)) continue;
+
+                int chkRouteCode = RouteFirst + i;
+                if (mapLayerData[mapController.CalcListWHConvNum(chkChipNum)] == chkRouteCode)
+                {
+                    // 繋がる情報を探せたら、そこに続くように現在の方向を設定する（そのまま入れれば大丈夫）
+                    routeData = (MapController.CsvCode)chkRouteCode;
+                    break;
+                }
+            }
+        }
+
+        // マップデータの進行方向を自分の進行方向に変換する
+        this.dir = (MapController.Dir)(routeData - RouteFirst);
+
+        // 進行方向にマップチップを、元の地点 or 折り返し地点に到達するまでチェックし続け、進行ルートとして保持する
+        int tmpDir = (int)this.dir;
+        while (true)
+        {
+            // 最後に保存した地点の添え字算出
+            int lastIdx = this.routeWHList.Count - 1;
+            // 最新の地点から現在の進行方向によって、次の地点を算出し、保持する
+            Vector2 nextChipNum = this.routeWHList[lastIdx] + MapController.RouteChkTbl[tmpDir];
+            // 進み先の地点のルート情報を取得し、次のチェックのために保持しておく
+            tmpDir = mapLayerData[mapController.CalcListWHConvNum(nextChipNum)] - RouteFirst;
+
+            // 進み先の地点がルート終了なのかチェック
+            // 元の地点かチェック
+            bool firstFlag = (this.routeWHList[0] == nextChipNum);
+            // 折り返し地点かチェック
+            bool returnFlag = (tmpDir == RouteReturn - RouteFirst);
+            // どちらの地点でもなく通常地点 or 折り返し地点なら
+            if ((!firstFlag && !returnFlag) || (returnFlag))
+            {
+                this.routeWHList.Add(nextChipNum);
+            }
+            if (firstFlag || returnFlag)
+            {
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 待機
+    /// </summary>
+    private void Stay()
+    {
+        this.time += Time.deltaTime;
+        if (this.time >= this.moveWaitTime)
+        {
+            this.stateRoutine1 = StateRoutine1.StateMove;
+            this.time = 0;
+        }
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        MapController mapController = MapController.Instance;
+        List<int> mapLayerData = mapController.GetMapDatas()[(int)MapController.MapLayerKind.Route];
+        int minNum = 0;
+        int maxNum = this.routeWHList.Count - 1;
+        this.nowRouteNum += ((this.routeDir) ? 1 : -1);
+        if (this.nowRouteNum < minNum)
+        {
+            this.nowRouteNum = maxNum;
+        }
+        if (this.nowRouteNum > maxNum)
+        {
+            this.nowRouteNum = minNum;
+        }
+        Vector2 wh = this.routeWHList[this.nowRouteNum];
+        int num = mapController.CalcListWHConvNum(wh);
+        MapController.CsvCode routeData = (MapController.CsvCode)mapLayerData[num];
+        if (routeData == MapController.CsvCode.RouteReturn)
+        {
+            this.routeDir = !this.routeDir;
+            this.dir += ( ((this.dir == MapController.Dir.Up) || (this.dir == MapController.Dir.Left) ? 1 : -1));
+        }
+        else
+        {
+            this.dir = (MapController.Dir)(routeData - MapController.CsvCode.RouteU);
+        }
+        this.transform.localPosition = mapController.CalcMapChipPos(wh);
+        this.stateRoutine1 = StateRoutine1.StateDefault;
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    protected override void Start()
+    {
+        base.Start();
+
+        // 関数テーブルに呼び出す関数を登録する
+        this.routineFuncList.Add(this.Stay);
+        this.routineFuncList.Add(this.Move);
+        this.stateRoutine1 = StateRoutine1.StateDefault;
+
+        // 経過時間初期化
+        this.time = 0.0f;
+        // 現在の位置は開始位置
+        this.nowRouteNum = 0;
+        // 進行方向を登録された順番で進むように設定
+        this.routeDir = true;
+
+        // ルート設定
+        this.SetRoute();
+    }
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    protected override void Update()
+    {
+        base.Update();
+
+        this.routineFuncList[(int)this.stateRoutine1]();
+    }
+}
---
+++ Assets/Script/Title/MapController.cs
@@ -0,0 +1,410 @@
+﻿/**
+* マップチップ管理クラス
+* @author Shota Funato
+*/
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using FunatoLib;
+
+public class MapController : SingletonMonoBehaviour<MapController>
+{
+    /// <summary>
+    /// マップレイヤーの種別
+    /// </summary>
+    public enum MapLayerKind
+    {
+        Map,
+        Wall,
+        Obj,
+        Route,
+
+        LayerMax
+    };
+
+    /// <summary>
+    /// 構成データの識別番号
+    /// </summary>
+    private enum CsvFirstLineData
+    {
+        WDataNum,
+        HDataNum,
+        ChipSizeW,
+        ChipSizeH,
+        LayerNum,
+        BitNum,
+
+        DataMax
+    };
+
+    /// <summary>
+    /// csv対応番号
+    /// </summary>
+    [Flags]
+    public enum CsvCode
+    {
+        None = 0,
+        AllWall,
+        Land,
+        Start,
+        Goal,
+        Warp1,
+        Warp2,
+        Warp3,
+        EmCat,
+        EmDog,
+
+        ItmCheese = 16,
+        GimickWeb,
+
+        WallU = 32,
+        WallR,
+        WallL,
+        WallD,
+        WallLR,
+        WallUD,
+        WallUL,
+        WallUR,
+        WallDR,
+        WallDL,
+        WallULR,
+        WallUDR,
+        WallDLR,
+        WallUDL,
+
+        RouteU = 48,
+        RouteD,
+        RouteL,
+        RouteR,
+        RouteReturn,
+
+        CodeEnd
+    };
+
+    /// <summary>
+    /// 方向列挙
+    /// </summary>
+    public enum Dir
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+
+        Max
+    };
+
+    /// <summary>
+    /// 通過できる方向を定義する構造体
+    /// </summary>
+    public struct PassageRoute
+    {
+        public bool[] dir;
+
+        public PassageRoute(bool u, bool d, bool l, bool r) : this()
+        {
+            dir = new bool[(int)MapController.Dir.Max];
+            dir[(int)MapController.Dir.Up] = u;
+            dir[(int)MapController.Dir.Down] = d;
+            dir[(int)MapController.Dir.Left] = l;
+            dir[(int)MapController.Dir.Right] = r;
+        }
+    };
+
+    /// <summary>
+    /// オブジェクト化するレイヤーデータテーブル
+    /// </summary>
+    public static readonly bool[] MapLayerFlagTbl = new bool[(int)MapController.MapLayerKind.LayerMax]
+    {
+        true,
+        true,
+        true,
+        false
+    };
+
+    /// <summary>
+    /// 壁があるマップから通過できる方向を定義したテーブル
+    /// </summary>
+    private static readonly PassageRoute[] PassageRouteTbl = new PassageRoute[(int)(MapController.CsvCode.WallUDL - MapController.CsvCode.WallU) + 2]
+    {
+        new PassageRoute( true, true, true, true ),
+        new PassageRoute( false, true, true, true ),
+        new PassageRoute( true, true, true, false ),
+        new PassageRoute( true, true, false, true ),
+        new PassageRoute( true, false, true, true ),
+        new PassageRoute( true, true, false, false ),
+        new PassageRoute( false, false, true, true ),
+        new PassageRoute( false, true, false, true ),
+        new PassageRoute( false, true, true, false ),
+        new PassageRoute( true, false, true, false ),
+        new PassageRoute( true, false, false, true ),
+        new PassageRoute( false, true, false, false ),
+        new PassageRoute( false, false, true, false ),
+        new PassageRoute( true, false, false, false ),
+        new PassageRoute( false, false, false, true ),
+    };
+
+    /// <summary>
+    /// 移動方向によってチェックする二次元配列添え字テーブル
+    /// </summary>
+    public static readonly Vector2[] RouteChkTbl = new Vector2[(int)MapController.Dir.Max]
+    {
+        Vector2.up * -1,
+        Vector2.down * -1,
+        Vector2.left,
+        Vector2.right,
+    };
+
+    /// <summary>
+    /// マップ全体のY軸オフセット値
+    /// </summary>
+    public static readonly float MapOffsetY = 0;
+
+    /// <summary>
+    /// マップチップの絵素材のサイズ
+    /// </summary>
+    public static readonly float MapChipPictSize = 64;
+
+    /// <summary>
+    ///  マップチップのスケール
+    /// </summary>
+    public static readonly float MapChipScale = 1.5f;
+
+    /// <summary>
+    /// マップチップのオブジェクトサイズ
+    /// </summary>
+    public static readonly float MapChipSize = MapController.MapChipPictSize * MapController.MapChipScale;
+
+    /// <summary>
+    /// CSVの最初の行に書いてあるデータをいれる。ステージの縦横幅、マップチップサイズ、レイヤー数がある
+    /// </summary>
+    private int[] csvFirstDatas = new int[(int)CsvFirstLineData.DataMax];
+
+    /// <summary>
+    /// CSVでレイヤー構造になっているステージ構成データをいれる
+    /// </summary>
+    private List<List<int>> mapDatas = new List<List<int>>();
+
+    /// <summary>
+    /// ステージ構成データ取得
+    /// </summary>
+    /// <returns>CsvFirstLineDataを要素番号として取得する</returns>
+    public int[] GetCsvFirstDatas()
+    {
+        return this.csvFirstDatas;
+    }
+
+    /// <summary>
+    /// 読み込んだマップデータ取得
+    /// </summary>
+    /// <returns></returns>
+    public List<List<int>> GetMapDatas()
+    {
+        return this.mapDatas;
+    }
+
+    /// <summary>
+    /// マップチップの縦軸個数取得
+    /// </summary>
+    /// <returns></returns>
+    public int GetMapHNum()
+    {
+        return this.csvFirstDatas[(int)MapController.CsvFirstLineData.HDataNum];
+    }
+
+    /// <summary>
+    /// マップチップの横軸個数取得
+    /// </summary>
+    /// <returns></returns>
+    public int GetMapWNum()
+    {
+        return this.csvFirstDatas[(int)MapController.CsvFirstLineData.WDataNum];
+    }
+
+    /// <summary>
+    /// マップのレイヤー数取得
+    /// </summary>
+    /// <returns></returns>
+    public int GetMapLayerNum()
+    {
+        return this.csvFirstDatas[(int)MapController.CsvFirstLineData.LayerNum];
+    }
+
+    /// <summary>
+    /// 二次元配列添え字から一次元配列添え字に変更
+    /// </summary>
+    /// <param name="w">横軸添え字</param>
+    /// <param name="h">縦軸添え字</param>
+    /// <returns>変換した添え字</returns>
+    public int CalcListWHConvNum(int w, int h)
+    {
+        return h * this.GetMapWNum() + w;
+    }
+
+    /// <summary>
+    /// 二次元配列添え字から一次元配列添え字に変更
+    /// </summary>
+    /// <param name="wh">縦横軸添え字</param>
+    /// <returns>変換した添え字</returns>
+    public int CalcListWHConvNum(Vector2 wh)
+    {
+        return this.CalcListWHConvNum((int)wh.x, (int)wh.y);
+    }
+
+    /// <summary>
+    /// 一次元配列添え字から二次元配列添え字に変更
+    /// </summary>
+    /// <param name="w">横軸添え字</param>
+    /// <param name="h">縦軸添え字</param>
+    /// <returns>変換した添え字</returns>
+    public Vector2 CalcListNumConvWH(int num)
+    {
+        return new Vector2(Mathf.FloorToInt(num / this.GetMapWNum()), num % this.GetMapWNum());
+    }
+
+    /// <summary>
+    /// 指定位置の通過可能方向取得
+    /// </summary>
+    /// <param name="wh"></param>
+    /// <returns></returns>
+    public PassageRoute GetPassageRoute(Vector2 wh)
+    {
+        int firstWallCode = (int)MapController.CsvCode.WallU;
+        int num = this.CalcListWHConvNum(wh);
+        num = this.mapDatas[(int)MapController.MapLayerKind.Wall][num];
+        if (num < firstWallCode)
+        {
+            return MapController.PassageRouteTbl[0];
+        }
+
+        return MapController.PassageRouteTbl[num - firstWallCode + 1];
+    }
+
+    /// <summary>
+    /// マップチップを並べて設置していく開始位置取得
+    /// </summary>
+    /// <returns>開始左上座標</returns>
+    public Vector2 GetMapChipStartPos()
+    {
+        Vector2 pos = new Vector2();
+        GameObject cameraObj = GameObject.Find("Main Camera");
+        Camera camera = cameraObj.GetComponent<Camera>();
+        float chipOfs = MapController.MapChipSize;
+        int maxH = this.GetMapHNum();
+        int maxW = this.GetMapWNum();
+        pos.x = camera.pixelWidth / 2 - chipOfs * (maxW - 1) / 2;
+        pos.y = camera.pixelHeight / 2 + chipOfs * (maxH - 1) / 2 + MapController.MapOffsetY;
+
+        return pos;
+    }
+
+    /// <summary>
+    /// 指定位置のマップチップ座標を算出して取得
+    /// </summary>
+    /// <param name="w">横軸番号</param>
+    /// <param name="h">縦軸番号</param>
+    /// <returns>マップチップ設置座標</returns>
+    public Vector2 CalcMapChipPos(int w, int h)
+    {
+        Vector2 pos = this.GetMapChipStartPos();
+        float chipOfs = MapController.MapChipSize;
+        pos.x += chipOfs * w;
+        pos.y -= chipOfs * h;
+
+        return pos;
+    }
+
+    /// <summary>
+    /// 指定位置のマップチップ座標を算出して取得
+    /// </summary>
+    /// <param name="w">縦横軸番号</param>
+    /// <returns>マップチップ設置座標</returns>
+    public Vector2 CalcMapChipPos(Vector2 wh)
+    {
+        Vector2 pos = this.GetMapChipStartPos();
+        float chipOfs = MapController.MapChipSize;
+        pos.x += chipOfs * wh.x;
+        pos.y -= chipOfs * wh.y;
+
+        return pos;
+    }
+
+    /// <summary>
+    /// 座標からマップチップ縦横軸番号算出して取得
+    /// </summary>
+    /// <returns>マップチップの縦横軸番号</returns>
+    public Vector2 CalcMapChipHW(Vector2 pos)
+    {
+        Vector2 mapChipHW = new Vector2();
+        Vector2 start_pos = this.GetMapChipStartPos();
+        float chipOfs = MapController.MapChipSize;
+        pos -= start_pos;
+        mapChipHW = pos / MapController.MapChipSize;
+        mapChipHW.y = -mapChipHW.y;
+
+        return mapChipHW;
+    }
+
+    /// <summary>
+    /// マップ生成
+    /// </summary>
+    /// <param name="fileName">読み込むcsvファイル名</param>
+    public void MapCreate(string fileName)
+    {
+        // csvデータを変換したものをいれるもの
+        TextAsset csvFile;
+        string[] dataList;
+
+        // リソース内のCsvフォルダにある指定ファイルを読み込む
+        csvFile = Resources.Load("Csv/" + fileName) as TextAsset;
+        StringReader reader = new StringReader(csvFile.text);
+
+        // 一番最初の行にあるデータはステージ構成に必要なデータなので、最初に取り出す
+        string firstLine = reader.ReadLine();
+        dataList = firstLine.Split(',');
+        for (int i = 0; i < this.csvFirstDatas.Length; i++)
+        {
+            int tmp = 0;
+            int.TryParse(dataList[i], out tmp);
+            this.csvFirstDatas[i] = tmp;
+        }
+
+        // csvで定義されたレイヤー分、Listを増やす
+        for (int i = 0; i < this.csvFirstDatas[(int)CsvFirstLineData.LayerNum]; i++)
+        {
+            this.mapDatas.Add(new List<int>());
+        }
+
+        // csvのレイヤーごとのデータをList内にいれる
+        int layerNum = 0;
+        int lineNum = 0;
+        while (reader.Peek() > -1)
+        {
+            // 行のデータを数値データに変換して保持する
+            string line = reader.ReadLine();
+            dataList = line.Split(',');
+            bool convFlag = false;
+            int tmp = 0;
+            for (int i = 0; i < dataList.Length; i++)
+            {
+                if (dataList[i].Equals("")) continue;
+                int.TryParse(dataList[i], out tmp);
+                this.mapDatas[layerNum].Add(tmp);
+                convFlag = true;
+            }
+
+            if (!convFlag) continue;
+
+            // 変換した行数を数える
+            lineNum++;
+
+            // １レイヤー分の行数を超えたら、入れる先を変更する
+            if (lineNum >= this.csvFirstDatas[(int)CsvFirstLineData.HDataNum])
+            {
+                layerNum++;
+                lineNum = 0;
+            }
+        }
+    }
+}
